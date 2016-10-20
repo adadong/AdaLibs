315c13b334f6c1d468fdca395b4bb37b7ce0959b
ada dong <fi_dongxueling@163.com>
2016-10-20 16:22:09 +0800

    ada

---
+++ app/src/main/java/com/ada/adalibs/activity/ShapeRadioButtonActivity.java
@@ -0,0 +1,15 @@
+package com.ada.adalibs.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ada.adalibs.R;
+
+public class ShapeRadioButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shape_radio_button);
+    }
+}
